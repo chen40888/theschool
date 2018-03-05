e31eec51073203d31808633a1e1ea2a9a426008a
Chen Nachum <chen40888@gmail.com>
2018-03-05 14:52:54 +0200

    +before templet

--- partials/students.partial.php
+++ partials/students.partial.php
@@ -1,5 +1,8 @@
-<li class="col-sm-3">
+<li class="student">
 	<a href="/student_details/<?php echo $id; ?>"><img src="<?php echo $image; ?>"></a>
-	<h2><?php echo $name; ?></h2>
-	<p><?php echo $phone; ?></p>
+	<div>
+		<h2><?php echo $name; ?></h2>
+		<p><?php echo $phone; ?></p>
+		<p><?php echo $email; ?></p>
+	</div>
 </li>
