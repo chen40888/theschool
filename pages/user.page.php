af89fa80449863f216c3663f69b832e5f7ebdc1c
Chen Nachum <chen40888@gmail.com>
2018-03-19 19:26:05 +0200

    +user page and change query for bouns

---
+++ pages/user.page.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chen
+ * Date: 19/03/2018
+ * Time: 15:14
+ */
---
+++ partials/one_user.partial.php
@@ -0,0 +1,14 @@
+<div class="col-sm-6 col-xs-12 course_show">
+	<a href="/edit_user/<?php echo $id?>"><img src="<?php echo $image;?>"></a>
+	<h3><?php echo $name . ' , ' .$role; ?></h3>
+	<p><?php echo $phone?></p>
+	<p><?php echo $email?></p>
+</div>
+
+<div>
+	<a href="/user/<?php echo $id?>"><img src="<?php echo $image;?>"></a>
+	<h3><?php echo $name . ' , ' .$role; ?></h3>
+	<p><?php echo $phone?></p>
+	<p><?php echo $email?></p>
+</div>
+
